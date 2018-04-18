29c670e7f19e5c46ebb7efa11ff14793fbe0c18d
Ryohei Ueda <garaemon@gmail.com>
2018-04-19 00:52:18 +0900

    Print progress in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -eu
 
-ampy --port /dev/tty.SLAB_USBtoUART put main.py
+echo "installing private.json"
 ampy --port /dev/tty.SLAB_USBtoUART put private.json
+echo "installing spotify.jpg"
 ampy --port /dev/tty.SLAB_USBtoUART put spotify.jpg
+echo "installing main.py"
+ampy --port /dev/tty.SLAB_USBtoUART put main.py
+echo "reset"
 ampy --port /dev/tty.SLAB_USBtoUART reset
